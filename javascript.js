edacee1ff6dbbba653a013f0f4a258c579425ee5
doug C windows <doug.cottrill@bellevuecollege.edu>
2018-04-21 19:25:54 -0700

    incl rain sprite, redo indents, all changes except left/right player

--- javascript.js
+++ javascript.js
@@ -1,80 +1,92 @@
-//PROG 219, HW-3 Rain
-//Written by Janusz Topor-Madry and Doug Cottrill
-//Last Modified 21 Apr 2018
-//A game using the Crafty.js system
-
 var screenWidth = 800;
-var screenHeight = 400;
+var screenHeight = 500;
 var hitCounter = 0;
 
-Crafty.init(screenWidth,screenHeight,document.getElementById('game'));
+Crafty.init(screenWidth,screenHeight, document.getElementById('game'));
 
-var player1 = Crafty.e('Player, 2D, Canvas, Color, Solid, Twoway, Gravity, Collision')
-  .attr({x: 20, y: 358, w: 16, h: 32})
-  .color('#F00').twoway(100).gravity('Floor')
-  .gravityConst(1000)
+Crafty.sprite("facio_sprite.gif", {facio:[237,0,30,55], facioLeft:[174,71,30,55], raindrop:[143,74,4,10]});
+Crafty.e('Floor, 2D, Canvas, Solid, Color, Collision')
+  .attr({x: 0, y: 480, w: screenWidth * 2, h: 20})
+  .color('lightblue');
+/*
+Crafty.e('ScreenSide, 2D, Canvas, Solid, Color, Collision')
+  .attr({x: 0, y: 480, w: screenWidth, h: 20})
+  .color('lightblue');
+*/
+var player1 = Crafty.e('Player, 2D, Canvas, Solid, Twoway, Gravity, Collision, facio')
+  .attr({x: 20, y: 460, w: 30, h: 55})
+  .twoway(150)
+  .gravity('Floor')
   .checkHits('LeftWall')
   .bind("HitOn", function(){    // prevent going past left edge and forever falling
-    player1.x = 4;              // (lower than 4 doesn't work right)
+    player1.x = 4;              // (=1 or =2 doesn't work right)
   })
   .bind("EnterFrame", function(){
-    if (this.x == screenWidth)
-      {
-        pause();
-        Crafty.e('2D, DOM, Text').attr({x:screenWidth/2, y:screenHeight/2}).text("Stage 1 Clear").textFont({size:'20px', weight:'bold'});
-      }
-   });
-
+    if (this.x >= screenWidth)
+    {
+      pause();
+      Crafty.e('2D, DOM, Text').attr({x:screenWidth/2 - 4, y:screenHeight/2}).text("Stage&nbsp;1 Clear")
+        .textFont({size:'20px', weight:'bold'}).textColor('#e0e0e0');
+    }
+  });
 var hitText = Crafty.e('2D, DOM, Text')
   .attr({
-    x: screenWidth - 140,
+    x: screenWidth - 100,
     y: 10
-});
+  });
 
 hitText.text('Hit:&nbsp;' + hitCounter);
+
 hitText.textFont({
-size: '30px',
-weight: 'bold'
-});
+  size: '30px',
+  weight: 'bold'
+  })
+  .textColor('#e0e0e0');
 
-Crafty.e('Floor, 2D, DOM, Color')
-  .attr({x: 0, y: 390, w: screenWidth, h: 10})
-  .color('lightblue');
+Crafty.e('LeftWall, 2D, DOM, Solid')
+  .attr({x: 0, y: 0, w: 1, h: screenHeight});
 
-Crafty.e('LeftWall, 2D, DOM, Color, Solid')
-.attr({x: 0, y: 0, w: 1, h: screenHeight})
-.color('#FFFFFF');
-
-function drop() {
-  var random_x = Math.floor(Math.random() * (screenWidth - 52) + 50); // - 52 to fit in screen
-  Crafty.e('Drop, 2D, Canvas, Color, Solid, Gravity, Collision')
-  .attr({x: random_x, y: 0, w: 2, h: 10})
-  .color('#000080')
+function drop()
+{
+  var randomx = Math.floor(Math.random() * (screenWidth - 54) + 50); // - 54 to fit in screen
+  Crafty.e('Drop, 2D, Canvas, Solid, Gravity, Collision, raindrop')
+  .attr({x: randomx, y: 0, w: 4, h: 10})
   .gravity()
   .gravityConst(200)
   .checkHits('Player')
-  .bind("EnterFrame", function() {
-      if (this.y > screenHeight)
-        this.destroy();
-  })
   .bind("HitOn", function(){
     this.destroy();
     hitCounter++;
-    hitText.text('Hit:&nbsp;' + hitCounter);
     if (hitCounter >= 6)
     {
       player1.x = 20;
       hitCounter = 0;
-      hitText.text("Hit: " + hitCounter);
     }
+    hitText.text('Hit:&nbsp;' + hitCounter);
+  })
+  .bind("EnterFrame", function() {
+    if (this.y > (screenHeight-30))
+      this.destroy();   // destroy object when near floor
   });
 }
 
-function pause() {
+function pause()
+{
+  var currMsg = document.getElementById("pauseButton").value;
+  if(currMsg=="pause"){
+    currMsg="continue";
+  }
+  else{
+    currMsg="pause";
+  }
+
+  document.getElementById("pauseButton").value = currMsg;
   Crafty.pause();
 }
 
-Crafty.bind("EnterFrame", function() {
-  if (Crafty.frame() % 4 == 0)
-    drop();
-})
+Crafty.bind("EnterFrame", function(){
+//(debug) document.getElementById("message").innerHTML = Crafty.frame();
+
+if (Crafty.frame() % 4 == 0)
+drop();
+});
